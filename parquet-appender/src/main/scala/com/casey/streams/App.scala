c7999bc39ee8ba99d64e67cda39e0b122b61ec15
Casey Hyde <hyde.casey@gmail.com>
2018-10-04 00:55:55 -0500

    add jenkinsfile

--- parquet-appender/src/main/scala/com/casey/streams/App.scala
+++ parquet-appender/src/main/scala/com/casey/streams/App.scala
@@ -5,18 +5,39 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 
 /**
- * @author ${user.name}
- */
+  * @author ${user.name}
+  */
 object App {
-  def main(args : Array[String]) {
+    def main(args: Array[String]) {
 
-        val source: String = try {args(0)} catch {case e: Exception => {println("must pass valid arg 1: source path"); throw new Exception("invalid arg1")}}
-        val output: String = try {args(1)} catch {case e: Exception => {println("must pass valid arg 2: output path"); throw new Exception("invalid arg2")}}
-        val appendType: String = try {args(2).toLowerCase} catch {case e: Exception => {println("must pass valid arg 3: appendType"); throw new Exception("invalid arg3")}}
+        val source: String = try {
+            args(0)
+        } catch {
+            case e: Exception => {
+                println("must pass valid arg 1: source path");
+                throw new Exception("invalid arg1")
+            }
+        }
+        val output: String = try {
+            args(1)
+        } catch {
+            case e: Exception => {
+                println("must pass valid arg 2: output path");
+                throw new Exception("invalid arg2")
+            }
+        }
+        val appendType: String = try {
+            args(2).toLowerCase
+        } catch {
+            case e: Exception => {
+                println("must pass valid arg 3: appendType");
+                throw new Exception("invalid arg3")
+            }
+        }
 
         if (appendType.toLowerCase() != "primary_key") {
             println("Sorry, I can only append primary keys to source parquets. For now...\n\n" +
-            "Add more functionality at https://github.com/30-day-challenge-october/stream-tools/parquet-appender")
+              "Add more functionality at https://github.com/30-day-challenge-october/stream-tools/parquet-appender")
             return
         }
         // this is needed... Don't worry, it can still run locally...
@@ -30,9 +51,11 @@ object App {
         val parquetDS = ParquetSource(parquetFilePath).toDataStream()
         val lastIndex = parquetDS.count
         println("parquet row count before append: " + lastIndex)
+        val schema = parquetDS.schema
+        println("schema: " + schema.toString)
         parquetDS.to(sink)
         println("parquet row count after append: " + lastIndex)
         println("done")
-  }
+    }
 
 }
